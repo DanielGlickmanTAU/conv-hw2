99339db0dd987c5eb1538bcf4ff3d8a3fed31384
Glickman <glickman1@mail.tau.ac.il>
2021-05-18 19:48:04 +0300

    implement mlp

--- part11.py
+++ part11.py
@@ -66,34 +66,73 @@ def test_cross_entropy():
     # Test backward pass
     test_block_grad(cross_entropy, scores, y=labels)
 
+def test_sequential():
+    # Test Sequential
+    # Let's create a long sequence of blocks and see
+    # whether we can compute end-to-end gradients of the whole thing.
+    seq = blocks.Sequential(
+        blocks.Linear(in_features, 100),
+        blocks.Linear(100, 200),
+        blocks.Linear(200, 100),
+        blocks.ReLU(),
+        blocks.Linear(100, 500),
+        blocks.Linear(500, 200),
+        blocks.ReLU(),
+        blocks.Linear(200, 500),
+        blocks.ReLU(),
+        blocks.Linear(500, 1),
+        blocks.Sigmoid(),
+    )
+    x_test = torch.randn(N, in_features)
+    # Test forward pass
+    z = seq(x_test)
+    test.assertSequenceEqual(z.shape, [N, 1])
+    # Test backward pass
+    test_block_grad(seq, x_test)
+
 # linear()
 # test_relu()
 # test_sigmoid()
 # test_cross_entropy()
+# test_sequential()
 
 
-# Test Sequential
-# Let's create a long sequence of blocks and see
-# whether we can compute end-to-end gradients of the whole thing.
+import hw2.models as models
 
-seq = blocks.Sequential(
-    blocks.Linear(in_features, 100),
-    blocks.Linear(100, 200),
-    blocks.Linear(200, 100),
-    blocks.ReLU(),
-    blocks.Linear(100, 500),
-    blocks.Linear(500, 200),
-    blocks.ReLU(),
-    blocks.Linear(200, 500),
-    blocks.ReLU(),
-    blocks.Linear(500, 1),
-    blocks.Sigmoid(),
-)
-x_test = torch.randn(N, in_features)
+# Create MLP model
+mlp = models.MLP(in_features, num_classes, hidden_features=[100, 50, 100])
+print(mlp)
 
-# Test forward pass
-z = seq(x_test)
-test.assertSequenceEqual(z.shape, [N, 1])
+# Test MLP architecture
+N = 100
+in_features = 10
+num_classes = 10
+for activation in ('relu', 'sigmoid'):
+    mlp = models.MLP(in_features, num_classes, hidden_features=[100, 50, 100], activation=activation)
+    test.assertEqual(len(mlp.sequence), 7)
 
-# Test backward pass
-test_block_grad(seq, x_test)
+    num_linear = 0
+    for b1, b2 in zip(mlp.sequence, mlp.sequence[1:]):
+        if (str(b2).lower() == activation):
+            test.assertTrue(str(b1).startswith('Linear'))
+            num_linear += 1
+
+    test.assertTrue(str(mlp.sequence[-1]).startswith('Linear'))
+    test.assertEqual(num_linear, 3)
+
+    # Test MLP gradients
+    # Test forward pass
+    x_test = torch.randn(N, in_features)
+    labels = torch.randint(low=0, high=num_classes, size=(N,), dtype=torch.long)
+    z = mlp(x_test)
+    test.assertSequenceEqual(z.shape, [N, num_classes])
+
+    # Create a sequence of MLPs and CE loss
+    # Note: deliberately using the same MLP instance multiple times to create a recurrence.
+    seq_mlp = blocks.Sequential(mlp, mlp, mlp, blocks.CrossEntropyLoss())
+    loss = seq_mlp(x_test, y=labels)
+    test.assertEqual(loss.dim(), 0)
+    print(f'MLP loss={loss}, activation={activation}')
+
+    # Test backward pass
+    test_block_grad(seq_mlp, x_test, y=labels)
